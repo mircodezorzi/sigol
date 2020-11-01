56cbbaebce10cf04b963db0397489b8608b23bce
Mirco De Zorzi <mircodezorzi@protonmail.com>
2020-11-02 00:44:21 +0100

    fix(utils): fix async issues

--- utils.go
+++ utils.go
@@ -7,23 +7,29 @@ import (
 
 func GitInit(path string) error {
 	cmd := exec.Command("git", "init", path)
-	return cmd.Start()
+	return cmd.Run()
 }
 
 func GoInit(path string, name string) error {
 	cmd := exec.Command("go", "mod", "init", name)
 	cmd.Dir = path
-	return cmd.Start()
+	return cmd.Run()
 }
 
 func GoGet(path string, pkg string) error {
 	cmd := exec.Command("go", "get", pkg)
 	cmd.Dir = path
-	return cmd.Start()
+	return cmd.Run()
 }
 
 func GoBuild(path string, fn string) error {
 	cmd := exec.Command("go", "build", "-o", "bin", fmt.Sprintf("./cmd/%s", fn))
 	cmd.Dir = path
-	return cmd.Start()
+	return cmd.Run()
+}
+
+func GoVendor(path string) error {
+	cmd := exec.Command("go", "mod", "vendor")
+	cmd.Dir = path
+	return cmd.Run()
 }
